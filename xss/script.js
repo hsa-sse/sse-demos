e0d81e1dc7ac7d3ab821f53223b3d54ced9e2fa4
Michael Netter <mail@michaelnetter.com>
2025-01-26 14:37:22 +0100

    Put script in script.js

---
+++ xss/script.js
@@ -0,0 +1,12 @@
+function updateGreeting() {
+    const hash = window.location.hash.substring(1);
+    const urlParams = new URLSearchParams(hash);
+    const name = urlParams.get('name');
+    document.getElementById('greeting').innerHTML = "<h2>Hello " + name + "</h2>";
+}
+
+// Execute the function on page load
+window.onload = updateGreeting;
+
+// Execute the function on fragment change
+window.onhashchange = updateGreeting;
